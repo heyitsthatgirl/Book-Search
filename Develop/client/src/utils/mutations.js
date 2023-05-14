7b46612656c6154d4b413c10a8aae1f821453a8e
hopemansfield <hopemansfield@gmail.com>
2023-05-14 14:55:45 -0400

    queries.js and mutations.js added to utils folder

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,45 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($userId: ID!, $savedBook: String!) {
+    saveBook(userId: $userId, savedBook: $savedBook) {
+      _id
+      username
+      savedBooks
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($savedBook: String!) {
+    removeBook(savedBook: $savedBook) {
+      _id
+      username
+      savedBooks
+    }
+  }
+`;
